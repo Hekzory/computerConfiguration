44f2c97ba1866c00de19bd1a89d3bfd76e85fb54
Oleg Tsvetkov <oleg-tsv@yandex.ru>
2024-06-08 23:30:21 +0300

    Update .wezterm.lua

--- linux/ansible/user_home/.wezterm.lua
+++ linux/ansible/user_home/.wezterm.lua
@@ -9,5 +9,28 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'tokyonight_night'
 
+config.window_frame = {
+
+  font = wezterm.font_with_fallback{'MesloLGS Nerd Font Mono', 'MesloLGS NF', 'JetBrainsMono Nerd Font Mono', 'JetBrains Mono'},
+  
+  font_size = 11.0,
+  
+}
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.use_fancy_tab_bar = true
+config.font = wezterm.font_with_fallback{'MesloLGS Nerd Font Mono', 'MesloLGS NF', 'JetBrainsMono Nerd Font Mono', 'JetBrains Mono'}
+config.enable_scroll_bar = true
+config.window_background_opacity = 0.975
+
+config.window_padding = {
+  left = '1cell',
+  right = '1cell',
+  top = '0.5cell',
+  bottom = '0.5cell',
+}
+
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+
 -- and finally, return the configuration to wezterm
 return config
