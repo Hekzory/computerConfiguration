95474f127505fd62adbb908235e2605996cc327e
Hekzory <oleg-tsv@yandex.ru>
2024-06-09 17:19:31 +0300

    Wezterm configuration reorganizaton

--- linux/ansible/user_home/.wezterm.lua
+++ linux/ansible/user_home/.wezterm.lua
@@ -1,42 +1,42 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local mux = wezterm.mux
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+-- Appearance settings
 config.color_scheme = 'tokyonight_night'
-
-config.term = "wezterm"
-
-config.window_frame = {
-	font = wezterm.font_with_fallback{'MesloLGS NF', 'MesloLGS Nerd Font Mono', 'JetBrainsMono Nerd Font Mono', 'JetBrains Mono'},
-	font_size = 11.0,
+config.font = wezterm.font_with_fallback{
+  'MesloLGS NF', 
+  'MesloLGS Nerd Font Mono', 
+  'JetBrainsMono Nerd Font Mono', 
+  'JetBrains Mono'
 }
-
+config.font_size = 11.0
+config.window_frame = {
+  font = config.font,
+  font_size = config.font_size,
+}
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.integrated_title_button_color = "Auto"
 config.use_fancy_tab_bar = true
-config.font = wezterm.font_with_fallback{'MesloLGS NF', 'MesloLGS Nerd Font Mono', 'JetBrainsMono Nerd Font Mono', 'JetBrains Mono'}
-config.enable_scroll_bar = true
 config.window_background_opacity = 0.975
-config.font_size = 11.0
+config.enable_scroll_bar = true
 config.use_resize_increments = true
 
+-- Window padding
 config.window_padding = {
-	left = '1cell',
-	right = '1cell',
-	top = '0.5cell',
-	bottom = '0.5cell',
+  left = '1cell',
+  right = '1cell',
+  top = '0.5cell',
+  bottom = '0.5cell',
 }
 
+-- Font features
 config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
+-- Backend settings
 config.prefer_egl=true
-
 config.front_end="WebGpu"
 config.webgpu_power_preference = "HighPerformance"
 
@@ -54,6 +54,9 @@ config.line_to_ele_shape_cache_size = 2048
 config.animation_fps = 60
 -- default is 60
 config.max_fps = 165
+
+-- Set terminal type for better integration
+config.term = "wezterm"
 
 -- and finally, return the configuration to wezterm
 return config
