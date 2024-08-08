b67e0e126bcf030e5f00f11d4ba7b91984cff80f
Oleg <oleg-tsv@yandex.ru>
2024-08-08 12:43:57 +1200

    added back completions

--- linux/ansible/user_home/.config/nvim/init.lua
+++ linux/ansible/user_home/.config/nvim/init.lua
@@ -52,8 +52,21 @@ lazy.setup({
 	{
 		"neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
 		lazy = false,
-		dependencies = {},
-		init = function() end,
+		dependencies = {
+			-- main one
+			{ "ms-jpq/coq_nvim", branch = "coq" },
+			-- 9000+ Snippets
+			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+			-- Need to **configure separately**
+			{ "ms-jpq/coq.thirdparty", branch = "3p" },
+		},
+		init = function()
+			vim.g.coq_settings = {
+				auto_start = "shut-up", -- if you want to start COQ at startup
+				-- Your COQ settings here
+			}
+		end,
 		config = function() end,
 	},
 	{
