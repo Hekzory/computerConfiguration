914304d2911dadb09395d1375f39f12fa0041726
Oleg Tsvetkov <o.tsvetkov@vk.team>
2025-05-14 02:22:24 +0300

    add buffer bars

--- linux/ansible/user_home/.config/nvim/init.lua
+++ linux/ansible/user_home/.config/nvim/init.lua
@@ -156,6 +156,23 @@ lazy.setup({
 		-- Auto pairs
 		{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
 		{ "folke/which-key.nvim", opts = {} },
+		{
+			"romgrk/barbar.nvim",
+			dependencies = {
+				"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+				"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+			},
+			init = function()
+				vim.g.barbar_auto_setup = false
+			end,
+			opts = {
+				-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+				animation = true,
+				tabpages = true,
+				clickable = true,
+			},
+			version = "^1.0.0", -- optional: only update when a new 1.x version is released
+		},
 	},
 })
 
