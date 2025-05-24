19ad900e763ab154c9678ea301f527626d72750a
Oleg Tsvetkov <o.tsvetkov@vk.team>
2025-05-24 13:09:57 +0300

    switch to nvim=cmp

--- linux/ansible/user_home/.config/nvim/init.lua
+++ linux/ansible/user_home/.config/nvim/init.lua
@@ -81,21 +81,80 @@ lazy.setup({
 			lazy = false,
 		},
 		{
-			"neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+			"neovim/nvim-lspconfig",
 			lazy = false,
+		},
+		{
+			"hrsh7th/nvim-cmp",
 			dependencies = {
-				-- main one
-				{ "ms-jpq/coq_nvim", branch = "coq" },
-				-- 9000+ Snippets
-				{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-				-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-				-- Need to **configure separately**
-				{ "ms-jpq/coq.thirdparty", branch = "3p" },
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-cmdline",
+				"L3MON4D3/LuaSnip",
+				"saadparwaiz1/cmp_luasnip",
 			},
-			init = function()
-				vim.g.coq_settings = { auto_start = "shut-up" }
+			config = function()
+				local cmp = require("cmp")
+				local luasnip = require("luasnip")
+
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							luasnip.lsp_expand(args.body)
+						end,
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.expand_or_jumpable() then
+								luasnip.expand_or_jump()
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+					}),
+					sources = cmp.config.sources({
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+					}, {
+						{ name = "buffer" },
+						{ name = "path" },
+					}),
+				})
+
+				-- Setup for command line
+				cmp.setup.cmdline({ "/", "?" }, {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = {
+						{ name = "buffer" },
+					},
+				})
+
+				cmp.setup.cmdline(":", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = "path" },
+					}, {
+						{ name = "cmdline" },
+					}),
+				})
 			end,
-			config = function() end,
 		},
 		{ "lambdalisue/vim-suda" },
 		{ "stevearc/conform.nvim", opts = {} },
@@ -248,8 +307,14 @@ require("nvim-treesitter.configs").setup({
 	highlight = { enable = true },
 })
 
-require("lspconfig").gopls.setup({})
-require("lspconfig").ruff.setup({})
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").gopls.setup({
+	capabilities = capabilities,
+})
+require("lspconfig").ruff.setup({
+	capabilities = capabilities,
+})
 
 require("conform").setup({
 	formatters = {
